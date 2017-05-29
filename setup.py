c5ae041ef0a70b4b9dec5a40463b2491a455b933
JamesMBartlett <james@ml.berkeley.edu>
2017-05-28 20:36:39 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='eig_state',
+      version='0.1',
+      description='Eigen State Manager',
+      author='James Bartlett',
+      author_email='jmabartlett@berkeley.edu',
+      packages=['eig_state']
+     )
